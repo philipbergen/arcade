38fd6dc56c0b1b6df137521f6cf4362de50f6429
Daniel <dan.gillet737@gmail.com>
2018-07-21 10:51:26 +0200

    Remove mock_window from all other unittest files

--- tests/unit/test_draw_commands.py
+++ tests/unit/test_draw_commands.py
@@ -1,4 +1,4 @@
-def test_rotate_point(mock_window):
+def test_rotate_point():
     from arcade import rotate_point
     x, y = rotate_point(1, 1, 0, 0, 90)
     assert (-1.0, 1.0) == (x, y)
--- tests/unit/test_sound.py
+++ tests/unit/test_sound.py
@@ -1,14 +1,14 @@
 import pytest
 
 
-def test_import(mock_window):
+def test_import():
     import arcade
     assert 'load' in arcade.load_sound.__name__
 
 
 @pytest.mark.not_ci
 @pytest.mark.parametrize('sound_format', ('wav', 'mp3', 'ogg'))
-def test_play_sound(mock_window, sound_format):
+def test_play_sound(sound_format):
     import arcade
     fn = 'tests/unit/laser1.' + sound_format
     my_sound = arcade.load_sound(fn)
