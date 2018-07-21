94be40eb96dd683a561cd25a53be4fe14e2d5a32
Daniel <dan.gillet737@gmail.com>
2018-07-21 10:16:19 +0200

    Different way of mocking pyglet.window.Window
    Only mock the relevant methods and assert they are called with the
    right parameters. Fixture pyglet_window will mock
    pyglet.window.Window.__init__ method, by-passing the instanciation
    of a real Window.

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,28 +1,10 @@
-import sys
-
 import pytest
+import pyglet
 
 
-class MockWindow:
-    """ Replace the pyglet base class with something we control """
-
-    def __init__(self, *args, **kwargs):
-        self.height = kwargs['height']
-        self.width = kwargs['width']
-        self.caption = kwargs['caption']
-        self.resizable = kwargs['resizable']
-        self._fullscreen = False
-
-    def set_fullscreen(self, value: bool):
-        self._fullscreen = value
-
-
-@pytest.fixture(autouse=True)
-def mock_window(monkeypatch):
-    sys.is_pyglet_docgen = True
-    monkeypatch.setattr('pyglet.window.Window', MockWindow)
-    from arcade import Window
-    return Window
+@pytest.fixture
+def pyglet_window(mocker):
+    yield mocker.patch.object(pyglet.window.Window, "__init__")
 
 
 @pytest.fixture
--- tests/unit/test_application.py
+++ tests/unit/test_application.py
@@ -1,66 +1,71 @@
+import pyglet
+import arcade
 
 
-def test_import(mock_window):
-    assert 'Window' == mock_window.__name__
+def test_import():
+    assert "Window" == arcade.Window.__name__
 
 
-def test_constructs(mock_window):
-    w = mock_window()
-    assert 'Window' == w.__class__.__name__
+def test_constructs(pyglet_window):
+    w = arcade.Window()
+    assert "Window" == w.__class__.__name__
 
 
-def test_initializes_defaults(mocker, mock_window):
-    mocker.patch.object(mock_window, 'set_update_rate')
-    w = mock_window()
-    assert 600 == w.height
-    assert 800 == w.width
-    assert 'Arcade Window' == w.caption
-    assert False is w._fullscreen
-    assert False is w.resizable
+def test_initializes_defaults(mocker, pyglet_window):
+    mocker.patch.object(pyglet.window.Window, "set_fullscreen")
+    mocker.patch.object(arcade.Window, "set_update_rate")
+    w = arcade.Window()
+    pyglet.window.Window.__init__.assert_called_once_with(
+        width=800,
+        height=600,
+        caption="Arcade Window",
+        resizable=False
+    )
+    pyglet.window.Window.set_fullscreen.assert_called_once_with(False)
     w.set_update_rate.assert_called_once_with(1 / 60)
     assert False is w.invalid
 
 
-def test_update(mock_window):
-    w = mock_window()
+def test_update(mocker, pyglet_window):
+    w = arcade.Window()
     assert None is w.on_update(1.1111)
 
 
-def test_set_update_rate_constructor(mock_window, pyglet_clock):
-    w = mock_window()
+def test_set_update_rate_constructor(pyglet_window, pyglet_clock):
+    w = arcade.Window()
     pyglet_clock.unschedule.assert_called_with(w.on_update)
     pyglet_clock.schedule_interval.assert_called_with(w.on_update, 1 / 60)
 
 
-def test_set_update_rate(mock_window, pyglet_clock):
-    w = mock_window()
+def test_set_update_rate(pyglet_window, pyglet_clock):
+    w = arcade.Window()
     w.set_update_rate(1 / 2)
     pyglet_clock.unschedule.assert_called_with(w.on_update, )
     pyglet_clock.schedule_interval.assert_called_with(w.on_update, 0.5)
 
 
-def test_on_mouse_motion(mock_window):
-    w = mock_window()
+def test_on_mouse_motion(pyglet_window):
+    w = arcade.Window()
     assert None is w.on_mouse_motion(0, 0, 0, 0)
 
 
-def test_on_mouse_press(mock_window):
-    w = mock_window()
+def test_on_mouse_press(pyglet_window):
+    w = arcade.Window()
     assert None is w.on_mouse_press(0, 0, 0, 0)
 
 
-def test_on_mouse_drag(mock_window, mocker):
-    w = mock_window()
-    mocker.patch.object(w, 'on_mouse_motion')
+def test_on_mouse_drag(pyglet_window, mocker):
+    w = arcade.Window()
+    mocker.patch.object(w, "on_mouse_motion")
     assert None is w.on_mouse_drag(0, 0, 0, 0, 0, 0)
     w.on_mouse_motion.assert_called_once_with(0, 0, 0, 0)
 
 
-def test_on_mouse_release(mock_window):
-    w = mock_window()
+def test_on_mouse_release(pyglet_window):
+    w = arcade.Window()
     assert None is w.on_mouse_release(0, 0, 0, 0)
 
 
-def test_on_mouse_scroll(mock_window):
-    w = mock_window()
+def test_on_mouse_scroll(pyglet_window):
+    w = arcade.Window()
     assert None is w.on_mouse_scroll(0, 0, 0, 0)
